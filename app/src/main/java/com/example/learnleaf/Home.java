a4a002d6140f766f0561f7ea4135f1dcee85e9c5
Benjamin Niccum <btn1609@mavs.uta.edu>
2024-09-18 12:54:52 -0500

    Added BG to Home, user sign out on home

--- app/src/main/java/com/example/learnleaf/Home.java
+++ app/src/main/java/com/example/learnleaf/Home.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
 
@@ -13,14 +16,16 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        Button main;
+        Button log_out = findViewById(R.id.logout);
 
-        main = findViewById(R.id.main);
 
-        main.setOnClickListener(v ->
-        {
+        log_out.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(Home.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(Home.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+            finish();
         });
     }
 }
