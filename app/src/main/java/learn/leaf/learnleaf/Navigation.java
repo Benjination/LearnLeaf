6d6744e7297215b61ff3b2d4e01011c888c3edbc
Benjamin Niccum <btn1609@mavs.uta.edu>
2025-01-17 11:03:48 -0600

    added blank line

--- app/src/main/java/learn/leaf/learnleaf/Navigation.java
+++ app/src/main/java/learn/leaf/learnleaf/Navigation.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Navigation extends Fragment {
